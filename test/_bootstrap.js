36f7a1b6894618afb75dfe7fa1a4566610ddbd4f
Alex Weber <alexweber15@gmail.com>
2018-07-17 15:26:46 -0300

    test: init testing framework

---
+++ test/_bootstrap.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+chai.should();
+
+global.expect = chai.expect;
+global.assert = chai.assert;
+global.fail = chai.assert.fail;
---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('module', function () {
+
+	// it('should expand firebase paths', function () {
+	// 	expect($firebase.expandPath('foo').path).to.equal('foo');
+	// 	expect($firebase.expandPath('foo/bar/baz').path).to.equal('foo/bar/baz');
+	// 	expect($firebase.expandPath(['foo']).path).to.equal('foo');
+	// 	expect($firebase.expandPath(['foo', 'bar', 'baz']).path).to.equal('foo/bar/baz');
+	// });
+
+});
